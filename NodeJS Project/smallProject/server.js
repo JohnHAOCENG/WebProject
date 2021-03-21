0c18a9ecb38a31414ef90b8d29d5289df943c85f
JohnHAOCENG <haoczhao@student.unimelb.edu.au>
2021-03-21 13:10:32 +0800

    heroku procfile

--- NodeJS Project/smallProject/server.js
+++ NodeJS Project/smallProject/server.js
@@ -1,13 +1,18 @@
 
 const express = require('express');
 const app = express();
-const port = 5000;
-
+const port =rocess.env.PORT || 5000;
+// for heroku const port = process.env.PORT || 5000
 //localhost:3000        /customer
 
 app.get('/', (req, res) => {
   res.send('Hello World! 牛逼啊')
 })
+
+app.get('/homepage', (req, res) => {
+  res.send('Hello World, welcome to home')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
