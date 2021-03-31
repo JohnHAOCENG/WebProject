ce3b02180787e3964f96a2b97430ad15d4e6fc67
JohnHAOCENG <haoczhao@student.unimelb.edu.au>
2021-03-31 21:04:48 +0800

    hard code html version

--- server.js
+++ server.js
@@ -1,64 +1,3 @@
-// //Food Buddy server using templates with injected data, hard-coded
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const exphbs = require('express-handlebars');
-// const mongoose = require('mongoose');
-// const app = express();
-// const port = process.env.PORT ||8080
-// app.use(bodyParser.urlencoded({extended:true}))
-
-// const db = require('./config/keys').mongoURL;
-// mongoose.
-// 	connect(db, { useNewUrlParser: true, useUnifiedTopology: true}).
-// 	then(()=>console.log("MongoDB Connected ..."));
-
-// app.engine('hbs', exphbs({
-// 	defaultlayout: 'main',
-// 	extname: 'hbs'
-// }))
-
-// app.set('view engine', 'hbs')
-
-
-// app.get('/', (req, res) => {  // /index.html
-// 	res.render('browse', {
-// 		foods: [
-// 			{id: 1, name: "Apple"},
-// 			{id: 2, name: "Bread"},
-// 			{id: 3, name: "Chocolate bar"},
-// 			{id: 4, name: "Dates (fresh)"}
-// 		]
-// 	})
-// })
-
-// app.get('/food/:id', (req, res) => {
-// 	res.render('showFood', {
-// 		foodId: req.params.id
-// 	})
-// })
-
-// app.get('/filter', (req, res) => {
-// 	res.render('filter')
-// })
-
-// app.post('/filter', (req, res) => { // receives POST data - user's food filter 
-// 	res.render('filterPost', {
-// 		filterData: JSON.stringify(req.body)
-// 	})
-// })
-
-// app.all('*', (req, res) => {  // 'default' route to catch user errors
-// 	res.status(404).render('404')
-// })
-
-// app.listen(port, () => {
-// 	console.log('Food Buddy server listening for requests ...')
-// })
-
-
-
-
-/**********************************************************************************/
 // Food Buddy server, with prototype HTML in the route handlers
 const express = require('express')
 const mongoose = require('mongoose');
@@ -72,31 +11,134 @@ mongoose.
 	connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true}).
 	then(()=>console.log("MongoDB Connected ..."));
 
-const customer = require("./routes/customer");
-const user = require("./routes/user");
 
-app.use('/customer', customer);
-app.use('/user', user);
+/* routers:  */
+//const customer = require("./routes/customer");
+//const user = require("./routes/user");
+/* app:  */
+//app.use('/customer', customer);
+//app.use('/user', user);
 
 app.get('/', (req, res) => {
 	res.status(200)
 	res.write('<h1> Home page </h1>')
-	res.write('<p> User can browse a list of foods </p>')
-	res.write('<ul>')
-	res.write('<li> <a href="/food/1">food 1</a> </li>')
-	res.write('<li> <a href="/food/2">food 2</a> </li>')
-	res.write('</ul>')
-	res.end('<a href="/filter"> set your food filter </a>')
+	res.write('<a href="/customer/login"><button>Login</button></a>')
+	res.write('&nbsp;')
+	res.write('<a href="/customer/register"><button>Register</button></a>')
+	res.write('</br>')
+	res.write('<p> Before we began</p>')
+	res.write('<p> Please tell us where you are </p>')
+	res.write('<textarea name="Location" id="" cols="50" rows="1"></textarea>')
+	res.write('</br>')
+	res.end('<a href="/customer"> <button> Confirm </button> </a>')
 })
 
 
-app.get('/food/:id', (req, res) => {
+
+app.get('/customer', (req, res) => {
+	res.status(200)
+	res.write('<h1> Home page </h1>')
+	res.write('<p> User can click on the location to the order page </p>')
+	res.write('<input type="button" value="Login"    onclick= <a href="customer/login/"></a>')
+	res.write('<input type="button" value="Register" onclick= <a href="customer/register"></a>')
+	res.write('<ul>')
+	res.write('<li> <a href="/order">location 1</a> </li>')
+	res.write('<li> <a href="/order">location 2</a> </li>')
+	res.write('<li> <a href="/order">location 3</a> </li>')
+	res.write('<li> <a href="/order">location 4</a> </li>')
+	res.write('<li> <a href="/order">location 5</a> </li>')
+	res.write('</ul>')
+	res.end('<p> please choose a nearby location </p>')
+})
+
+
+
+app.get('/customer/login', (req, res) => {
+	res.status(200)
+	res.write('<h1> Login Page </h1>')
+	res.write('<li> <a href="/">Back to Home Page</a> </li>')
+	res.write('<a href="/customer/register"><button>Register</button></a>')
+	res.write('</br>')
+	res.write('User-name: </br>')
+	res.write('<textarea name="user-name" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('Password: </br>')
+	res.write('<textarea name="pass-word" id="" cols="50" rows="1"></textarea>')
+	res.write('<p> Forget your password? </p>')
+	res.end('<a href="/customer"> <button> LOG IN </button> </a>')
+})
+
+
+
+app.get('/customer/register', (req, res) => {
+	res.status(200)
+	res.write('<h1> Register Page </h1>')
+	res.write('<li> <a href="/">Back to Home Page</a> </li>')
+	res.write('Family Name: </br>')
+	res.write('<textarea name="family name" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('Given Name: </br>')
+	res.write('<textarea name="given word" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('Email Address: </br>')
+	res.write('<textarea name="email address" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('Password: </br>')
+	res.write('<textarea name="password" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('<p> must include letter and number, 8~16 digits</p>')
+	res.write('Phone Number: </br>')
+	res.write('<textarea name="phone number" id="" cols="50" rows="1"></textarea> </br>')
+	res.write('Verify Code: </br>')
+	res.write('<textarea name="verify code" id="" cols="50" rows="1"></textarea> </br>')
+	res.end('<a href="/customer"> <button> CREATE </button> </a>')
+})
+
+
+app.get('/order', (req, res) => {
+	res.status(200)
+	res.write('<h1> Details of the order </h1>')
+	res.write('<li> <a href="/">Back to Home Page</a> </li>')
+	res.write('<p> The user clicked on a food. Display details of that food here. </p>')
+	res.write('<li> <a href="/order/cappuccino">		Cappuccino		</a> </li>')
+	res.write('<li> <a href="/order/latte">			Latte     		</a> </li>')
+	res.write('<li> <a href="/order/flatwhite">		Flat White		</a> </li>')
+	res.write('<li> <a href="/order/longblack">  	Long black		</a> </li>')
+	res.write('<li> <a href="/order/fancybiscuit">	Fancy biscuit 	</a></li>')
+	res.write('<li> <a href="/order/smallcake">	    Small cake		</a></li>')
+	res.write('<li> <a href="/order/bigcake">	    Big cake		</a></li>')
+	res.write('<li> <a href="/order/plainbiscuit">   Plain biscuit	</a></li>')
+	res.write('</br>')
+	res.end('<p> <a href="/order/confirm">			place the order	 </a> </p>')
+})
+
+
+app.get('/order/confirm', (req, res) => {
+	res.status(200)
+	res.write('<h1> Order Derails </h1>')
+	res.write('<li> <a href="/">				    Back to Home Page		</a> </li>')
+	res.write('<p> Order No: </p>')
+	res.write('<p> Address: </p>')
+	res.write('<p> Food Details: </p>')
+	res.write('<p> Price: </p>')
+	res.write('<p> Status: </p>')
+	res.write('<p> Timer: </p>')
+	res.write('<li> <a href="/order/ongoing">Confirm and Pay</a> </li>')
+	res.end('<li> <a href="/order"> Cancel </a> </li>')
+})
+
+app.get('/order/order/ongoing', (req, res) => {
+	res.status(200)
+	// wait for you
+})
+
+
+app.get('/order/:snack', (req, res) => {
 	res.status(200)
 	res.write('<h1> Details of one food </h1>')
 	res.write('<p> The user clicked on a food. Display details of that food here. </p>')
-	res.end('<p> Requested food id is: ' + req.params.id + '</p>')
+	res.write('<li> <a href="/order">Back to order</a> </li>')
+	res.end('<p> Requested food is: ' + req.params.snack + '</p>')
 })
 
+
+
+/*------------------------------------------------------------------- */
 app.get('/filter', (req, res) => {
 	res.status(200)
 	res.write('<h1> Food filter </h1>')
